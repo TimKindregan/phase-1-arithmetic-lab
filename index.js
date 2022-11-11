2e67c02e99b9e2f57dd549329d743091af76eb9e
Tim <timothyj.kindregan@gmail.com>
2022-11-11 14:41:49 -0500

    commit tree 184f6218684cf5a6238946eed3f55781479f11cd

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1;
+let num2 = 62;
+let num3 = 19;
+let num4 = 5;
 
-// Write your code here
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3%num4;
+
+let max = Math.max(1, 5, 19, 20);
